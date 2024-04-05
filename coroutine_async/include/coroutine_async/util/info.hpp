95b860c160f374bb42297aed39799b975622ae57
XenoWYC121 <1299288319@qq.com>
2024-04-05 15:29:01 +0800

    ADD:添加类 read_info

--- coroutine_async/include/coroutine_async/util/info.hpp
+++ coroutine_async/include/coroutine_async/util/info.hpp
@@ -35,23 +35,47 @@ namespace coroutine_async::util
             : public info
     {
     public:
-        accept_info(int fd, sockaddr_in &addr, int &sock_fd, int &ec)
+        accept_info(int fd, sockaddr_in& addr, int& sock_fd, int& ec)
                 : info(info_type::ACCEPT), fd(fd), addr(&addr), sock_fd(&sock_fd), ec(&ec) {}
 
         int get_fd() const { return this->fd; }
 
-        sockaddr_in &get_sock_addr() { return *this->addr; }
+        sockaddr_in& get_sock_addr() { return *this->addr; }
 
-        int &get_sock_fd() { return *this->sock_fd; }
+        int& get_sock_fd() { return *this->sock_fd; }
 
-        int &get_ec() { return *this->ec; }
+        int& get_ec() { return *this->ec; }
 
     private:
         int fd;
-        sockaddr_in *addr;
-        int *sock_fd;
-        int *ec;
+        sockaddr_in* addr;
+        int* sock_fd;
+        int* ec;
+    };
 
+    class read_info
+            : public info
+    {
+    public:
+        read_info(int fd, char* buffer, size_t& size, int& ec, size_t res_size)
+                : info(info_type::READ), fd(fd), buffer(buffer), size(&size), ec(&ec), res_size(&res_size) {}
+
+        int get_fd() const { return this->fd; }
+
+        char* get_buffer() { return this->buffer; }
+
+        size_t& get_size() { return *this->size; }
+
+        int& get_ec() { return *this->ec; }
+
+        size_t& get_res_size() { return *this->res_size; }
+
+    private:
+        int fd;
+        char* buffer;
+        size_t* size;
+        int* ec;
+        size_t* res_size;
     };
 
 }
